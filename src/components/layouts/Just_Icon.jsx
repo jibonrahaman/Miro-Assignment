8fb1d3e74c4adc65b6965a3754dfca074fde86e3
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-03-03 01:50:39 +0600

    Just_Icon layout design done!

--- src/components/layouts/Just_Icon.jsx
+++ src/components/layouts/Just_Icon.jsx
@@ -1,9 +1,11 @@
+import Svg from "../common/Svg";
 
 export default function Just_Icon() {
   return (
      // 3nd part of project
-  <section className="max-w-container mx-auto">
+  <section className="max-w-container mx-auto pt-16  px-28">
   <p className=" text-xl text-[#787694] text-center">Trusted by 45M+ users</p>
+  <Svg  className=" flex justify-between pt-5 "/>
   </section>
   )
 }
