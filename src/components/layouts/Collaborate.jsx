78dfcb37605146f62c85651556d008f2306ad132
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-03-03 15:14:47 +0600

    Collaborate layout page Header text

--- src/components/layouts/Collaborate.jsx
+++ src/components/layouts/Collaborate.jsx
@@ -1,12 +1,14 @@
-import Images from "../common/Images";
-import starts from '/images/starts.png'
+import CollaborateSvg from "../common/CollaborateSvg";
+
 export default function Collaborate() {
   return (
          // 4nd part of project
-   <section className=" max-w-container mx-auto px-28">
-      <div className=" flex justify-center ml-96" >
-      <Images src={starts} alt={starts}   />
+   <section className=" max-w-container mx-auto px-28 pt-24">
+      <div className=" flex text-center  justify-center relative" >
+      <h2 className=" text-5xl w-[45%]  font-bold font-inter ">Collaborate without constraints</h2>
+      <CollaborateSvg className=" absolute top-[-71px] right-64 "/>
       </div>
+      
    </section>
   )
 }
