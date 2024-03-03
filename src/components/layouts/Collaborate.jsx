77ae6b465817ac32f2a005b78adb2c77af0736cd
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-03-03 15:28:43 +0600

    Collaborate layout page Card design done

--- src/components/layouts/Collaborate.jsx
+++ src/components/layouts/Collaborate.jsx
@@ -1,4 +1,5 @@
 import CollaborateSvg from "../common/CollaborateSvg";
+import Flex from "../common/Flex";
 
 export default function Collaborate() {
   return (
@@ -8,7 +9,22 @@ export default function Collaborate() {
       <h2 className=" text-5xl w-[45%]  font-bold font-inter ">Collaborate without constraints</h2>
       <CollaborateSvg className=" absolute top-[-71px] right-64 "/>
       </div>
-      
+       <Flex className=" justify-between pt-10">
+        <div className=" w-80">
+     <h3 className=" font-bold text-2xl font-inter">Free forever</h3>
+      <p className=" text-lg text-[#696688] mt-4">Our free plan gives you unlimited team members, 3 boards, and 300+ expert-made templates. Signing up with your work email lets you bring in your team faster. See our </p>
+<p className="text-lg text-[#696688]"><span className=" text-blue">pricing plans</span> for more features.</p>
+     </div>
+     <div className=" w-80">
+     <h3 className=" font-bold text-2xl font-inter">Easy integrations</h3>
+      <p className=" text-lg text-[#696688] mt-4 ">Miro has 100+ powerful integrations with tools you already use like G Suite, Slack, and Jira, so your workflow is seamless. View the full list in our <span className=" text-blue">Marketplace</span> . </p>
+     </div>
+     <div className=" w-80">
+     <h3 className=" font-bold text-2xl font-inter">Security first</h3>
+      <p className=" text-lg text-[#696688] mt-4  ">We treat your data like you would — with the utmost care. We follow industry-leading security standards and give you tools to protect intellectual property. Learn more 
+at our  <span className=" text-blue">Trust Center</span> . </p>
+     </div>
+          </Flex>
    </section>
   )
 }
