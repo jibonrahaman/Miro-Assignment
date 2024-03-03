8fa1d4fdc43845df0457b4804c03117dc098980a
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-03-03 23:02:48 +0600

    Ways  layout design  done!

--- src/components/layouts/Ways.jsx
+++ src/components/layouts/Ways.jsx
@@ -3,9 +3,14 @@ import Images from '../common/Images'
 // import Flex from '../common/Flex'
 export default function Ways() {
   return (
-    <section className=" max-w-container mx-auto">
-     <div>
+    <section className=" max-w-container mx-auto pt-14">
+     <div className=' flex items-end justify-end relative'>
      <Images src={ways} alt={ways}/>
+     <div  className=' absolute top-[40%] right-10 text-center'>
+     <h3 className=' text-3xl font-bold font-inter'>The Ways We Work</h3>
+     <p className=' font-medium my-2'>How has our relationship with work changed?</p>
+     <button className=' px-4 py-2  border border-gray-400  bg-blue text-white hover:bg-transparent duration-300  hover:text-black rounded-full'>View the report →</button>
+     </div>
      </div>
     </section>
   )
