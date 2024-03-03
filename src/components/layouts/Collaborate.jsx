ec3b1bfeea2369f28155e13ca5ea8b64ca53b6ec
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-03-03 14:51:53 +0600

    Collaborate layout page.

--- src/components/layouts/Collaborate.jsx
+++ src/components/layouts/Collaborate.jsx
@@ -1,9 +1,12 @@
-
+import Images from "../common/Images";
+import starts from '/images/starts.png'
 export default function Collaborate() {
   return (
          // 4nd part of project
-   <section>
-    
+   <section className=" max-w-container mx-auto px-28">
+      <div className=" flex justify-center ml-96" >
+      <Images src={starts} alt={starts}   />
+      </div>
    </section>
   )
 }
