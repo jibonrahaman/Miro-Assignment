61a601d743b69a717c224247524a7a2401e08f79
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-03-03 21:38:31 +0600

    Work layout design

---
+++ src/components/layouts/Work.jsx
@@ -0,0 +1,12 @@
+import Flex from '../common/Flex'
+export default function Work() {
+  return (
+    <section className=" max-w-container mx-auto px-28">
+      <Flex>
+       <div>
+        <h2>Work together, wherever you work</h2>
+       </div>
+      </Flex>
+    </section>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Collaborate from "../components/layouts/Collaborate";
 import Ideas from "../components/layouts/Ideas";
 import Just_Icon from "../components/layouts/Just_Icon";
 import Nav from "../components/layouts/Nav";
+import Work from "../components/layouts/Work";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
     <Ideas />
     <Just_Icon />
     <Collaborate />
+    <Work/>
     </div>
   )
 }
