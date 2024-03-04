2348ab5b3096e104741f1c1f010a192bbd50384e
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-03-04 14:31:30 +0600

    Built layout design done!

--- src/components/layouts/Built.jsx
+++ src/components/layouts/Built.jsx
@@ -1,5 +1,7 @@
  import Heading from '../common/Heading'
  import Flex from '../common/Flex'
+ import Images from '../common/Images'
+ import built from '/images/built.png'
 export default function Built() {
   return (
     <section className=" max-w-container mx-auto  pt-24">
@@ -7,12 +9,20 @@ export default function Built() {
     <Flex className=' justify-between pt-8'>
     <button className=' px-4 py-2 rounded-full bg-transparent hover:bg-gray-300 duration-700 border '>Brainstorming</button>
     <button className=' px-4 py-2 rounded-full bg-transparent hover:bg-gray-300 duration-700 border '>Diagramming</button>
-    <button className=' px-3 py-2 rounded-full bg-transparent hover:bg-gray-300 duration-700 border '>Meetings & Workshops</button>
+    <button className=' px-4 py-2 rounded-full bg-transparent hover:bg-gray-300 duration-700 border '>Meetings & Workshops</button>
     <button className=' px-4 py-2 rounded-full bg-transparent hover:bg-gray-300 duration-700 border '>Scrum Events</button>
     <button className=' px-4 py-2 rounded-full bg-transparent hover:bg-gray-300 duration-700 border '>Mapping</button>
-    <button className=' px-3 py-2 rounded-full bg-transparent hover:bg-gray-300 duration-700 border '>Research & Design</button>
-    <button className=' px-3 py-2 rounded-full bg-transparent hover:bg-gray-300 duration-700 border '>Strategic Planning</button>
+    <button className=' px-4 py-2 rounded-full bg-transparent hover:bg-gray-300 duration-700 border '>Research & Design</button>
+    <button className=' px-4 py-2 rounded-full bg-transparent hover:bg-gray-300 duration-700 border '>Strategic Planning</button>
     </Flex>
+      <Flex className='justify-between pt-5'>
+         <div className=' pt-7'>
+          <h4 className=' text-lg font-medium'>Brainstorming</h4>
+          <p className=' text-lg text-[#504d74] pt-5 pb-8 w-72'>Unleash creative ideas and build on them with the help of sticky notes, images, mind maps, videos, drawing capabilities — the list goes on.</p>
+          <button className=' text-blue font-medium'>Learn more →</button>
+         </div>
+         <Images src={built} alt={built} />
+      </Flex>
     </section>
   )
 }
