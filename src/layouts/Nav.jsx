f7d892fd95619d6937f2e58ba6a7b1a17ba2c9a4
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-03-05 19:19:14 +0600

    responsive nav

--- src/layouts/Nav.jsx
+++ src/layouts/Nav.jsx
@@ -11,8 +11,8 @@ export default function Nav() {
       <nav className="md:px-28 mx-auto  md:pt-5 font-inter font-normal"> 
       <Flex className='items-center justify-between'>
         <Flex className="md:gap-x-6 items-center">
-        <Images src={miroLogo} alt={miroLogo} className=' w-14 md:w-full' />
-          <Flex className="md:gap-x-5 mt-2">
+        <Images src={miroLogo} alt={miroLogo} className=' w-16 md:w-full' />
+          <Flex className="md:gap-x-5 gap-x-2 mt-2">
             <ul className="flex md:gap-x-5 text-[10px]">
               <Flex className="items-center gap-x-1">
               <li className="relative after:absolute after:content-[''] after:bottom-[-5px] after:left-[50%] hover:after:left-0 hover:after:right-0 after:w-0 after:h-1 after:rounded-md after:bg-black/70 duration-700 after:duration-700 hover:after:w-full">Product</li>
