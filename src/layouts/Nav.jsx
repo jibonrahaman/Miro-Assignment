bce0ced7de8be2c4a3ebae76a13ca53db3e2fe0c
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-03-05 19:26:39 +0600

    responsive nav

--- src/layouts/Nav.jsx
+++ src/layouts/Nav.jsx
@@ -3,17 +3,21 @@ import Flex from '../common/Flex'
 import miroLogo from '/images/miroLogo.png'
 import { FaAngleDown } from "react-icons/fa";
 import { TbWorld } from "react-icons/tb";
+import { FaBars } from "react-icons/fa";
 
 export default function Nav() {
+  const handleBars = ()=>{
+    console.log("dd");
+  }
   return (
      // 1nd part of project
     <section>
-      <nav className="md:px-28 mx-auto  md:pt-5 font-inter font-normal"> 
+      <nav className="md:px-28 mx-auto pt-3  md:pt-5 font-inter font-normal"> 
       <Flex className='items-center justify-between'>
-        <Flex className="md:gap-x-6 items-center">
+        <Flex className="md:gap-x-6 gap-x-2 items-center">
         <Images src={miroLogo} alt={miroLogo} className=' w-16 md:w-full' />
-          <Flex className="md:gap-x-5 gap-x-2 mt-2">
-            <ul className="flex md:gap-x-5 text-[10px]">
+          <Flex className="gap-x-5  mt-2">
+            <ul className="flex md:gap-x-5 gap-x-2 text-[10px]">
               <Flex className="items-center gap-x-1">
               <li className="relative after:absolute after:content-[''] after:bottom-[-5px] after:left-[50%] hover:after:left-0 hover:after:right-0 after:w-0 after:h-1 after:rounded-md after:bg-black/70 duration-700 after:duration-700 hover:after:w-full">Product</li>
                 <FaAngleDown />
@@ -31,8 +35,10 @@ export default function Nav() {
             </ul>
           </Flex>
         </Flex>
+        
+        <div className=' hidden md:block'>
         <Flex className='items-center gap-x-4 mt-2'>
-          <Flex className='items-center gap-x-1'>
+          <Flex className='items-center gap-x-1 '>
             <TbWorld />
             <h4>EN</h4>
           </Flex>
@@ -40,6 +46,11 @@ export default function Nav() {
           <button>Login</button>
           <button className="px-5 py-2 text-center bg-blue text-white rounded-full border hover:bg-transparent hover:text-blue duration-300">Sign up free →</button>
         </Flex>
+        </div>
+        {/* mobile device */}
+         <div className=' md:hidden block mt-2 pr-2 text-xl'>
+        <button onClick={handleBars}><FaBars/></button>
+         </div>
       </Flex>
     </nav>
     <div className=" border mt-8"></div>
