16647877eb29ad5d3a0e6e4265faf1915e046c06
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-03-05 19:46:01 +0600

    responsive nav bars

--- src/layouts/Nav.jsx
+++ src/layouts/Nav.jsx
@@ -4,6 +4,7 @@ import miroLogo from '/images/miroLogo.png'
 import { FaAngleDown } from "react-icons/fa";
 import { TbWorld } from "react-icons/tb";
 import { FaBars } from "react-icons/fa";
+import { RxCross1 } from "react-icons/rx";
 
 export default function Nav() {
   const handleBars = ()=>{
@@ -12,7 +13,7 @@ export default function Nav() {
   return (
      // 1nd part of project
     <section>
-      <nav className="md:px-28 mx-auto pt-3  md:pt-5 font-inter font-normal"> 
+      <nav className="md:px-28 mx-auto pt-3 relative  md:pt-5 font-inter font-normal"> 
       <Flex className='items-center justify-between'>
         <Flex className="md:gap-x-6 gap-x-2 items-center">
         <Images src={miroLogo} alt={miroLogo} className=' w-16 md:w-full' />
@@ -37,7 +38,7 @@ export default function Nav() {
         </Flex>
         
         <div className=' hidden md:block'>
-        <Flex className='items-center gap-x-4 mt-2'>
+        <Flex className='items-center gap-x-2 mt-2'>
           <Flex className='items-center gap-x-1 '>
             <TbWorld />
             <h4>EN</h4>
@@ -48,8 +49,18 @@ export default function Nav() {
         </Flex>
         </div>
         {/* mobile device */}
-         <div className=' md:hidden block mt-2 pr-2 text-xl'>
+         <div className=' md:hidden  block mt-2 pr-6 text-xl'>
         <button onClick={handleBars}><FaBars/></button>
+        <div className=' absolute top-0 right-0 bg-gray-600 text-white h-screen w-[50%] text-center pt-5 px-5'>
+        <button className=' absolute top-1 text-2xl text-red-600 right-1'><RxCross1 /></button>
+          <Flex className='items-center pt-6 text-center justify-center gap-x-1 '>
+            <TbWorld />
+            <h4>EN</h4>
+          </Flex>
+          <h4 className=' py-4'>Contact Sales</h4>
+          <button>Login</button>
+          <button className="my-4 px-3 py-2 text-center bg-blue text-white rounded-full border hover:bg-transparent hover:text-blue duration-300">Sign up free →</button>
+        </div>
          </div>
       </Flex>
     </nav>
