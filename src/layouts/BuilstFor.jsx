7bf5257aef326f2fd0acb1d3b23d5d30fae67c0f
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-03-07 20:07:05 +0600

    BuiltsFor  layout responsive

--- src/layouts/BuilstFor.jsx
+++ src/layouts/BuilstFor.jsx
@@ -12,7 +12,7 @@ export default function BuilstFor() {
   return (
     <section className=" max-w-container mx-auto  pt-24">
     <Heading text='Built for all kinds of teams'/>
-    <Flex className=' justify-between pt-8'>
+    <Flex className=' md:justify-between gap-x-5 justify-center gap-y-3 flex-wrap pt-8'>
     <button className=' px-4 py-2 rounded-full bg-transparent hover:bg-gray-300 duration-700 border '>UX & Design</button>
     <button className=' px-4 py-2 rounded-full bg-transparent hover:bg-gray-300 duration-700 border '>Marketing</button>
     <button className=' px-4 py-2 rounded-full bg-transparent hover:bg-gray-300 duration-700 border '>Product Management</button>
@@ -22,7 +22,7 @@ export default function BuilstFor() {
     <button className=' px-4 py-2 rounded-full bg-transparent hover:bg-gray-300 duration-700 border '>Sales</button>
     </Flex>
 
-      <Flex className='justify-between pt-5'>
+      <Flex className='md:justify-between   flex-wrap pt-5'>
          <div className=' pt-10'>
          <Flex className=' items-center  pb-4 gap-x-2'>
             <MdCheck/>
@@ -37,16 +37,17 @@ export default function BuilstFor() {
          <p className=' text-lg text-[#504d74] '>Run engaging design workshops</p>
          </Flex>
           <button className=' text-blue font-medium pb-8'>Learn more →</button>
-          <h5 className='text-[#504d74] '>Integrate your favorite tools</h5>
+         <div className='flex md:flex-none items-center md:items-start flex-col'>
+         <h5 className='text-[#504d74] '>Integrate your favorite tools</h5>
           <Flex className='gap-x-4 pt-5'>
            <Images src={d} alt={d}/>
            <Images src={x} alt={x}/>
            <Images src={f} alt={f}/>
            <Images src={n} alt={n}/>
-          
           </Flex>
          </div>
-         <Images src={builtFor} alt={builtFor} className='w-3/5'/>
+         </div>
+         <Images src={builtFor} alt={builtFor} className='md:w-3/5'/>
       </Flex>
     </section>
   )
