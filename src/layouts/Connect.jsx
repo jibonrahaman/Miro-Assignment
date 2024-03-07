aef290f528fcc095682761bb03a3d3ebeb0fc2dd
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-03-07 19:28:21 +0600

    Connect  layout responsive

--- src/layouts/Connect.jsx
+++ src/layouts/Connect.jsx
@@ -3,12 +3,12 @@ import Images from '../common/Images'
 import connect from '/images/connect.png'
 export default function Connect() {
   return (
-    <section className=" max-w-container mx-auto ">
-       <Flex className=' justify-between items-center'>
-          <Images src={connect} alt={connect} className='w-[45%]' />
-          <div className=' w-[45%]'>
-            <h2 className='w-[75%] leading-tight text-5xl font-bold font-inter flex flex-col'>Connect  <span>your tools, close your tabs</span></h2>
-            <p className='mt-4 text-[#696688] text-lg w-[85%]'>Whether you want to edit your Google Docs, resolve Jira issues, or collaborate over Zoom, Miro has 100+ integrations with tools you already use and love.</p>
+    <section className=" max-w-container mx-auto pt-12 md:pt-0">
+       <Flex className=' justify-between flex-col md:flex-row gap-y-5 items-center'>
+          <Images src={connect} alt={connect} className='md:w-[45%]' />
+          <div className=' md:w-[45%]'>
+            <h2 className='md:w-[75%] leading-tight text-2xl md:text-5xl font-bold font-inter flex flex-col'>Connect  <span>your tools, close your tabs</span></h2>
+            <p className='mt-4 text-[#696688] text-lg  md:w-[85%]'>Whether you want to edit your Google Docs, resolve Jira issues, or collaborate over Zoom, Miro has 100+ integrations with tools you already use and love.</p>
             <button className='mt-6 text-blue underline'>Learn more →</button>
           </div>
          </Flex>
