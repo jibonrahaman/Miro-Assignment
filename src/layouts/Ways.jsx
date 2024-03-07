7402d7dac7aacc8f776f153ff4a90f352134d290
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-03-07 19:40:44 +0600

    Ways  layout responsive

--- src/layouts/Ways.jsx
+++ src/layouts/Ways.jsx
@@ -2,12 +2,12 @@ import ways from '/images/ways.png'
 import Images from '../common/Images'
 export default function Ways() {
   return (
-    <section className=" px-28  pt-14">
+    <section className=" md:px-28  pt-14">
      <div className=' flex items-end justify-end relative'>
-     <Images src={ways} alt={ways}/>
-     <div  className=' absolute top-[40%] right-10 text-center'>
-     <h3 className=' text-3xl font-bold font-inter'>The Ways We Work</h3>
-     <p className=' font-medium my-2'>How has our relationship with work changed?</p>
+     <Images src={ways} alt={ways} className=' '/>
+     <div  className=' absolute top-10 md:top-[40%] right-[-30px] md:right-10 text-center'>
+     <h3 className=' md:text-3xl font-bold font-inter'>The Ways We Work</h3>
+     <p className=' md:font-medium text-[8px] md:text-base w-64 md:w-full my-2'>How has our relationship with work changed?</p>
      <button className=' px-4 py-2  border border-gray-400  bg-blue text-white hover:bg-transparent duration-300  hover:text-black rounded-full'>View the report →</button>
      </div>
      </div>
