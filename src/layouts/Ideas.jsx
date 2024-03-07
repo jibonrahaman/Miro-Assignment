74da2a662d3a2205c2b2d63c7345e0d8eda8c1ba
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-03-07 18:59:31 +0600

    Ideas layout responsive

--- src/layouts/Ideas.jsx
+++ src/layouts/Ideas.jsx
@@ -10,15 +10,15 @@ import Heading from '../common/Heading'
 export default function Ideas() {
   return (
     // 2nd part of project
-    <section className=" max-w-container mx-auto mt-16 font-inter ">
+    <section className=" max-w-container mx-auto mt-10 md:mt-16 font-inter ">
       <Flex className=" justify-between">
-        <div className=' flex flex-col  w-[450px] '>
+        <div className=' flex flex-col   md:w-[450px] '>
           <Heading text='Take ideas from better to best' />
           <p className=" text-xl my-3">Miro is your team's visual platform to connect, collaborate, and create — together.</p>
           <input className=' border rounded-full w-full px-10 py-3 hover:bg-blue duration-500 hover:placeholder:text-white outline-none ' type="text" placeholder="Enter your work email" />
           <button className=" bg-blue text-white border rounded-full w-full px-10 py-3 hover:bg-transparent duration-500 hover:text-black my-3">Sign up free →</button>
           <p className=' text-[#9b99af]'>Collaborate with your team within minutes</p>
-          <Flex className=" gap-x-8 mt-4 py-3 px-3  bg-[#f5f5f7] rounded-md">
+          <Flex className="hidden md:block gap-x-8 mt-4 py-3 px-3  bg-[#f5f5f7] rounded-md">
             <div>
               <Flex className=" text-[#FF9D48] text-2xl gap-x-3">
                 <MdOutlineStarPurple500 />
@@ -36,7 +36,7 @@ export default function Ideas() {
             </Flex>
           </Flex>
         </div>
-        <div>
+        <div className=' hidden md:block'>
           <Images src={banner} alt={banner} className='w-[550px]  ' />
         </div>
       </Flex>
